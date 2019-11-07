8e346c8c4cfe8f2142a0d5d63f2b5503440dc5e7
Joshua Cox <joshdcox50@gmail.com>
2019-11-07 15:11:12 -0500

    updated styling for mainpage to styled components.  Also began work on SVG for main page button

--- src/Components/mainpage.jsx
+++ src/Components/mainpage.jsx
@@ -1,27 +1,64 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
-import '../styles/mainpage.css';
+import styled, {createGlobalStyle} from 'styled-components';
+import bgimg from '../images/crookednumber6.jpg';
+import Playball from './svg components/playball';
+
+
+const Globalstyle = createGlobalStyle`
+    body {
+        background-image: url(${bgimg});
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+        background-color: #464646;
+        height: 100vh;
+        font-family: "Courier New", Courier, monospace;
+}
+`;
+
+const TitleContainer = styled.div` 
+    height: 100vh;
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: space-evenly;
+    align-content: center;
+    align-items: center;
+`;
+const SiteTitle = styled.h1`
+    margin: 0px;
+    padding-right: 0px;
+    padding-left: 0px; 
+    margin-top: 30vh;
+    background: black;
+    font-family: "Courier New", Courier, monospace;
+    text-decoration: none;
+    color: #004a7c;
+    font-size: 1.5em;
+    padding: 10px 0px 10px 0px;
+`;
+
+
+
 
 
 class Mainpage extends React.Component{
     render(){
         return(
             <div>
-            
-                <div className="main-body">
-                    <div className="background-image">
-                        <div className="header-wrapper">
-                            <div className="title">
-                                <h1>Crooked Number Fantasy Baseball</h1>
-                            </div>
-                            <button className="login-button" ><Link to='/login'>Login</Link></button>
-                        </div>
-                        
-                      
-                    </div>
-                </div>
-
+                <Globalstyle></Globalstyle>
+                <TitleContainer>
+                    <SiteTitle>Crooked Number Fantasy Baseball</SiteTitle>
+                    <Link to='/login'><Playball /></Link>
+                </TitleContainer>
+                 
             </div>
             
         )
